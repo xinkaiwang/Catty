a0870fd8b76d63dbe8b840e622815920d04c3154
Xinkai Wang <aakwxk@hotmail.com>
2015-05-25 10:51:56 -0700

    Added empty project for test server and client

---
+++ Catty/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catty
+{
+    public class Class1
+    {
+        public static String Hello = "Hello";
+    }
+}
